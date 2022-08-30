074c9818a2c4cd8dcfbd116c279e991f80c5711b
Goban <gobanest@gmail.com>
2022-08-30 20:35:04 +0900

    feat custom button

--- DataScannerUIKit/ViewController.swift
+++ DataScannerUIKit/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     lazy var dataScannerController: DataScannerViewController = {
         let viewController =  DataScannerViewController(recognizedDataTypes: [.text()],qualityLevel: .accurate, recognizesMultipleItems: false, isHighFrameRateTrackingEnabled: false, isPinchToZoomEnabled: true, isGuidanceEnabled: true, isHighlightingEnabled: true)
         viewController.delegate = self
+        
+        viewController.view.addSubview(catchButton)
+        catchButton.addTarget(self, action: #selector(captureText), for: .touchUpInside)
+        catchButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            catchButton.centerXAnchor.constraint(equalTo: viewController.view.centerXAnchor),
+            catchButton.bottomAnchor.constraint(equalTo: viewController.view.bottomAnchor, constant: -100),
+            catchButton.widthAnchor.constraint(equalToConstant: 110),
+            catchButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
         return viewController
     }()
     lazy var scanButton: UIButton = {
@@ -23,62 +33,103 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(startScanning), for: .touchUpInside)
         return button
     }()
-    var catchText: UILabel = {
+    lazy var catchButton: UIButton = {
+        let button = UIButton()
+        button.configuration = .filled()
+        button.setTitle("Catch", for: .normal)
+        button.addTarget(self, action: #selector(captureText), for: .touchUpInside)
+        button.isUserInteractionEnabled = false
+        button.configuration?.background.backgroundColor = .gray
+        return button
+    }()
+    var catchLabel: UILabel = {
         let label = UILabel()
         label.text = "none"
         label.textColor = .red
-        label.numberOfLines = 10
+        label.numberOfLines = 20
         return label
     }()
+    
+    var currentItems: [RecognizedItem.ID: String] = [:] {
+        didSet {
+            if currentItems.isEmpty {
+                catchButton.isUserInteractionEnabled = false
+                catchButton.configuration?.background.backgroundColor = .gray
+            } else {
+                catchButton.isUserInteractionEnabled = true
+                catchButton.configuration?.background.backgroundColor = .systemBlue
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(catchButton.isUserInteractionEnabled)
         configureUI()
     }
     
     func configureUI() {
         view.backgroundColor = .white
         view.addSubview(scanButton)
-        view.addSubview(catchText)
+        view.addSubview(catchLabel)
         scanButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             scanButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             scanButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -30),
         ])
         
-        catchText.translatesAutoresizingMaskIntoConstraints = false
+        catchLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            catchText.topAnchor.constraint(equalTo: scanButton.bottomAnchor, constant: 30),
-            catchText.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            catchText.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            catchLabel.topAnchor.constraint(equalTo: scanButton.bottomAnchor, constant: 30),
+            catchLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            catchLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
         ])
-        
     }
     
     @objc func startScanning() {
         if DataScannerViewController.isSupported && DataScannerViewController.isAvailable {
-//            navigationController?.pushViewController(dataScannerController, animated: true)
+            //            navigationController?.pushViewController(dataScannerController, animated: true)
             present(dataScannerController, animated: true)
             try? self.dataScannerController.startScanning()
         }
     }
+    
+    @objc func captureText() {
+        guard let item = currentItems.first else { return } // recognizesMultipleItems 를 사용하지않기 떄문에 하나만 선택
+        catchLabel.text = item.value
+        dataScannerController.dismiss(animated: true)
+        dataScannerController.stopScanning()
+    }
 }
 
 extension ViewController: DataScannerViewControllerDelegate {
-    func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
-        switch item {
-        case .text(let text):
-            print(text.transcript.split(separator: "\n"))
-//            print(text.transcript)
-            catchText.text = text.transcript
-        case .barcode(let barcode):
-            print(barcode.payloadStringValue ?? "unkown")
-            catchText.text = barcode.payloadStringValue ?? "unkown"
-        default:
-            break
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+        for item in addedItems {
+            switch item {
+            case .text(let text):
+                currentItems[item.id] = text.transcript
+            default:
+                break
+            }
         }
-        dataScanner.dismiss(animated: true)
-        dataScanner.stopScanning()
+    }
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didUpdate addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+        for item in addedItems {
+            switch item {
+            case .text(let text):
+                currentItems[item.id] = text.transcript
+            default:
+                break
+            }
+        }
+    }
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didRemove addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+        for item in addedItems {
+            currentItems.removeValue(forKey: item.id)
+        }
     }
 }
